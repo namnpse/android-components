7e0e241cd9a3d9779c60e9fefc3c5bf22fda01c3
nam444479@gmail.com <nam.nguyen19@onemount.com>
2023-07-20 17:41:19 +0700

    [Content Provider] access and display images via content provider

---
+++ app/src/main/java/com/namnp/androidcomponents/content_provider/ImageViewModel.kt
@@ -0,0 +1,16 @@
+package com.namnp.androidcomponents.content_provider
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class ImageViewModel: ViewModel() {
+
+    var images by mutableStateOf(emptyList<Image>())
+        private set
+
+    fun updateImages(images: List<Image>) {
+        this.images = images
+    }
+}
